372fd3f934aece7326f964b49d39558a91ce6e89
maiAbdulhameed <mai.abdelhameed@rowaad.net>
2023-10-22 17:27:12 +0300

    auth

--- src/app/[locale]/api/route.ts
+++ src/app/[locale]/api/route.ts
@@ -2,9 +2,9 @@ import { setCookie } from "cookies-next";
 import { NextApiRequest, NextApiResponse } from "next";
 import requestIp from 'request-ip';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: any, res: any) => {
   const detectedIp = requestIp.getClientIp(req)
-  
+
   setCookie("user-ip", detectedIp);
   localStorage.setItem('user-ip', JSON.stringify(detectedIp))
   res.status(200).json(detectedIp);
