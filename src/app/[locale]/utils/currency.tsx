3ecfbe3bc8e5dbff639023300fad2d0d49e14d6b
MaiAbdulhamid <mai.abdelhameed95@gmail.com>
2024-03-01 23:57:21 +0200

    Wallet

--- src/app/[locale]/utils/currency.tsx
+++ src/app/[locale]/utils/currency.tsx
@@ -4,18 +4,20 @@ import { useEffect, useState } from "react";
 
 export const Format = (value: number | string = "") => {
   const locale = useLocale();
-  const [userCountry, setUserCountry] = useState<any>({})
+  const [userCountry, setUserCountry] = useState<any>({});
+
   useEffect(() => {
     const getUserCountry = async () => {
       const response = await getCountry();
       setUserCountry(response)
     }
     getUserCountry()
-  }, [])
+  }, []);
+
   return (
     <span className="formatted--value">
       {value || 0}
-      {" " + userCountry[`currency_${locale}`]  || null}
+      {" " + userCountry[`currency_${locale}`] || ''}
     </span>
   );
 };
