0cb169566d0538ec428cb832f3da78dccdb53fb4
MaiAbdulhamid <mai.abdelhameed95@gmail.com>
2024-03-21 03:23:28 +0200

    add Rate

--- src/app/[locale]/products/[id]/DetailsPage/components/Reviews/index.tsx
+++ src/app/[locale]/products/[id]/DetailsPage/components/Reviews/index.tsx
@@ -1,7 +1,7 @@
 import { ReviewWrapper } from "./style";
 import { P4 } from "@/app/[locale]/components/Typography";
 import EnNumber from "@/app/[locale]/utils/EnNumber";
-import { Rating as MantineRationg } from "@mantine/core";
+import { Rating as MantineRating } from "@mantine/core";
 import { Flex } from "@/app/[locale]/components/Grids";
 import { useDisclosure } from "@mantine/hooks";
 import ReviewsModal from "./ReviewsModal";
@@ -13,27 +13,28 @@ export default function Reviews({ rate, product }: any) {
     { open: openProductReviews, close: closeProductReviews },
   ] = useDisclosure(false);
 
-
   if (+rate === 0) return null;
 
   return (
-    <Button noStyle  onClick={openProductReviews}>
-      <ReviewWrapper>
-        <P4>
-          {/* {Array.from({ length: 5 }).map((star, index) => (
+    <>
+      <Button noStyle onClick={openProductReviews}>
+        <ReviewWrapper>
+          <P4>
+            {/* {Array.from({ length: 5 }).map((star, index) => (
             <StarIcon key={`star=${index}`} />
           ))} */}
-          <Flex align="center">
-            <MantineRationg defaultValue={rate} readOnly />
-            <EnNumber> ({rate}) </EnNumber>
-          </Flex>
-        </P4>
-        <ReviewsModal
-          productId={product.id}
-          opened={openedProductReviews}
-          onClose={closeProductReviews}
-        />
-      </ReviewWrapper>
-    </Button>
+            <Flex align="center">
+              <MantineRating defaultValue={rate} readOnly />
+              <EnNumber> ({rate}) </EnNumber>
+            </Flex>
+          </P4>
+        </ReviewWrapper>
+      </Button>
+      <ReviewsModal
+        productId={product.id}
+        opened={openedProductReviews}
+        onClose={closeProductReviews}
+      />
+    </>
   );
 }
