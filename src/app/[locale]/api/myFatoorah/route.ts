b598ffc064bdc971e1a9be2a5043b4c921ba2179
MaiAbdulhamid <mai.abdelhameed95@gmail.com>
2024-06-23 21:42:10 +0300

    Fix payment

--- src/app/[locale]/api/myFatoorah/route.ts
+++ src/app/[locale]/api/myFatoorah/route.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import { NextRequest } from "next/server";
 
 // MyFatoorah API credentials
-const SECURE_KEY =
-  "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL";
+const SECURE_KEY = process.env.NEXT_PUBLIC_MYFATOORAH_SECURE_KEY;
 
 export async function POST(request: NextRequest) {
   const { totalAmount, phoneNumber, countryCode, currency, customerName } =
@@ -16,8 +15,9 @@ export async function POST(request: NextRequest) {
     CustomerName: customerName,
     MobileCountryCode: countryCode,
     DisplayCurrencyIso: currency,
-    CallBackUrl: "https://trendy-eta.vercel.app/api/myfatoorah-callback",
-    // ErrorUrl: "http://localhost:3000/error",
+    CallBackUrl:
+      process.env.CALLBACK_URL ||
+      "http://localhost:3000/api/myfatoorah-callback",
   };
 
   const headers = {
@@ -27,12 +27,10 @@ export async function POST(request: NextRequest) {
 
   try {
     const response = await axios.post(
-      "https://apitest.myfatoorah.com/v2/SendPayment",
+      "https://api.myfatoorah.com/v2/SendPayment",
       paymentData,
       { headers }
     );
-
-    console.log("MyFatoorah response:", response.data);
 
     const paymentUrl = response.data.Data.InvoiceURL;
     // Send the payment URL back to the client
@@ -42,7 +40,6 @@ export async function POST(request: NextRequest) {
 
     if (axios.isAxiosError(error)) {
       console.error("Error response data:", error.response?.data);
-      console.error("Error response status:", error.response?.status);
       return new Response(
         JSON.stringify({
           message: "Payment error",
--- src/app/[locale]/api/myfatoorah-callback/route.ts
+++ src/app/[locale]/api/myfatoorah-callback/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
-const SECURE_KEY =
-  "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL";
+
+const SECURE_KEY = process.env.NEXT_PUBLIC_MYFATOORAH_SECURE_KEY;
 
 export async function GET(request: NextRequest) {
   // you can get `headers` `searchParams` `params` etc. from this request object
@@ -13,11 +13,12 @@ export async function GET(request: NextRequest) {
   };
   try {
     const response = await axios.post(
-      "https://apitest.myfatoorah.com/v2/getPaymentStatus",
+      "https://api.myfatoorah.com/v2/getPaymentStatus",
       {
         key: searchParams.get("paymentId"),
         keyType: "paymentId",
-        ErrorUrl: "https://trendy-eta.vercel.app/error",
+        ErrorUrl:
+          process.env.NEXT_PUBLIC_ERROR_URL || "http://localhost:3000/error",
       },
       { headers }
     );
@@ -25,7 +26,7 @@ export async function GET(request: NextRequest) {
     console.log(response.data.Data.InvoiceId);
     // Send the payment URL back to the client
     return NextResponse.redirect(
-      `https://trendy-eta.vercel.app/checkout?invoiceId=${response.data.Data.InvoiceId}`
+      `http://localhost:3000/checkout?invoiceId=${response.data.Data.InvoiceId}`
     );
   } catch (error) {
     console.error("MyFatoorah Callback error:", error);
